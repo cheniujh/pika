5f41972dc1293be8a034ae9197264a5844f48694
cjh <1271435567@qq.com>
2024-06-17 20:59:18 +0800

    add metric 'is_eligible_for_master_election' to indicate whether the instance has corrupted full sync, which can be used in codis-pika cluster reelection scenario

--- include/pika_conf.h
+++ include/pika_conf.h
@@ -421,6 +421,7 @@ class PikaConf : public pstd::BaseConf {
   int64_t rsync_timeout_ms() {
       return rsync_timeout_ms_.load(std::memory_order::memory_order_relaxed);
   }
+
   // Slow Commands configuration
   const std::string GetSlowCmd() {
     std::shared_lock l(rwlock_);
@@ -769,12 +770,48 @@ class PikaConf : public pstd::BaseConf {
   }
 
   int64_t cache_maxmemory() { return cache_maxmemory_; }
+
   void SetSlowCmd(const std::string& value) {
     std::lock_guard l(rwlock_);
     std::string lower_value = value;
     pstd::StringToLower(lower_value);
     TryPushDiffCommands("slow-cmd-list", lower_value);
     pstd::StringSplit2Set(lower_value, ',', slow_cmd_set_);
+  }
+
+  void SetInternalUsedUnFinishedFullSync(const std::string& value) {
+    std::lock_guard l(rwlock_);
+    std::string lower_value = value;
+    pstd::StringToLower(lower_value);
+    TryPushDiffCommands("internal-used-unfinished-full-sync", lower_value);
+    pstd::StringSplit2Set(lower_value, ',', internal_used_unfinished_full_sync_);
+  }
+
+  void AddInternalUsedUnfinishedFullSync(const std::string& db_name) {
+    {
+      std::lock_guard l(rwlock_);
+      internal_used_unfinished_full_sync_.insert(db_name);
+      std::string lower_value = pstd::Set2String(internal_used_unfinished_full_sync_, ',');
+      pstd::StringToLower(lower_value);
+      TryPushDiffCommands("internal-used-unfinished-full-sync", lower_value);
+    }
+    ConfigRewrite();
+  }
+
+  void RemoveInternalUsedUnfinishedFullSync(const std::string& db_name) {
+    {
+      std::lock_guard l(rwlock_);
+      internal_used_unfinished_full_sync_.erase(db_name);
+      std::string lower_value = pstd::Set2String(internal_used_unfinished_full_sync_, ',');
+      pstd::StringToLower(lower_value);
+      TryPushDiffCommands("internal-used-unfinished-full-sync", lower_value);
+    }
+    ConfigRewrite();
+  }
+
+  size_t GetUnfinishedFullSyncCount() {
+    std::shared_lock l(rwlock_);
+    return internal_used_unfinished_full_sync_.size();
   }
 
   void SetCacheType(const std::string &value);
@@ -945,6 +982,9 @@ class PikaConf : public pstd::BaseConf {
   int throttle_bytes_per_second_ = 207200000;
   int max_rsync_parallel_num_ = kMaxRsyncParallelNum;
   std::atomic_int64_t rsync_timeout_ms_ = 1000;
+
+  //Internal used metrics Persisted by pika.conf
+  std::unordered_set<std::string> internal_used_unfinished_full_sync_;
 };
 
 #endif
